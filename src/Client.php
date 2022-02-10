55f664e266097dfe2792a365d37b9b6ab7c74596
greksazoltan <zoli.greksa@gmail.com>
2022-02-10 14:10:41 +0100

    update phpstan

--- src/Client.php
+++ src/Client.php
@@ -2,25 +2,25 @@
 	
 	namespace Greksazoo\MnbExchangeLaravel;
 	
-	
 	use Greksazoo\MnbExchangeLaravel\Model\Currency;
 	use SoapClient;
 	
-	class Client {
-		
+	class Client
+	{
 		/**
 		 * @var SoapClient
 		 */
 		protected $client;
 		
 		/**
-		 * Client constructor.
+		 * Client
+		 * constructor.
 		 *
 		 * @param string $wsdl
 		 *
 		 * @throws \SoapFault
 		 */
-		public function __construct($wsdl = 'http://www.mnb.hu/arfolyamok.asmx?wsdl')
+		public function __construct ($wsdl = 'http://www.mnb.hu/arfolyamok.asmx?wsdl')
 		{
 			$this->client = new SoapClient($wsdl);
 		}
@@ -28,7 +28,7 @@
 		/**
 		 * @param SoapClient $client
 		 */
-		public function setClient(SoapClient $client)
+		public function setClient (SoapClient $client): void
 		{
 			$this->client = $client;
 		}
@@ -36,35 +36,37 @@
 		/**
 		 * @return string[]
 		 */
-		public function currencies()
+		public function currencies ()
 		{
 			return (array)simplexml_load_string($this->client->GetCurrencies(null)->GetCurrenciesResult)->Currencies->Curr;
 		}
 		
 		/**
 		 * @param string $currency
+		 *
 		 * @return boolean
 		 */
-		public function hasCurrency($currency)
+		public function hasCurrency ($currency)
 		{
 			return in_array($currency, $this->currencies());
 		}
 		
 		/**
 		 * @param Currency|string $code
-		 * @param null $date
+		 * @param null            $date
+		 *
 		 * @return Currency|null
 		 */
-		public function currentExchangeRate($code, &$date = null)
+		public function currentExchangeRate ($code, &$date = null)
 		{
 			$code = trim(strtoupper($code));
-			
-			foreach($this->currentExchangeRates($date) as $currency) {
+			foreach ($this->currentExchangeRates($date) as $currency)
+			{
 				
-				if($currency->getCode() == $code) {
+				if ($currency->getCode() == $code)
+				{
 					return $currency;
 				}
-				
 			}
 			
 			return null;
@@ -72,38 +74,31 @@
 		
 		/**
 		 * @param null $date
+		 *
 		 * @return array|Currency[]
 		 */
-		public function currentExchangeRates(&$date = null)
+		public function currentExchangeRates (&$date = null)
 		{
 			$xml = simplexml_load_string($this->client->GetCurrentExchangeRates(null)->GetCurrentExchangeRatesResult);
-			
 			$date = (string)$xml->Day->attributes()->date;
-			
 			$currencies = [];
-			foreach ($xml->Day->Rate as $rate) {
+			foreach ($xml->Day->Rate as $rate)
+			{
 				
-				$currencies[] = new Currency(
-					(string)$rate->attributes()->curr,
-					(int)$rate->attributes()->unit,
-					(float)str_replace(',', '.', $rate)
-				);
+				$currencies[] = new Currency((string)$rate->attributes()->curr, (int)$rate->attributes()->unit, (float)str_replace(',', '.', $rate));
 			}
 			
 			return $currencies;
 		}
 		
 		/**
-		 * Proxy for SOAP client
+		 * @param string $name
+		 * @param mixed  $arguments
 		 *
-		 * @param $name
-		 * @param $arguments
 		 * @return mixed
 		 */
-		public function __call($name, $arguments)
+		public function __call ($name, $arguments)
 		{
 			return $this->client->{$name}(...$arguments);
 		}
-		
-		
 	}
--- src/MnbExchangeLaravel.php
+++ src/MnbExchangeLaravel.php
@@ -7,10 +7,24 @@
 	
 	class MnbExchangeLaravel
 	{
+		/**
+		 * @var Client
+		 */
 		protected $client;
+		/**
+		 * @var Repository
+		 */
 		protected $cache;
+		/**
+		 * @var int|mixed
+		 */
 		protected $timeout;
 		
+		/**
+		 * @param Client     $client
+		 * @param Repository $cache
+		 * @param int|mixed $timeout
+		 */
 		public function __construct (Client $client, Repository $cache, $timeout=1440)
 		{
 			$this->client = $client;
@@ -18,11 +32,19 @@
 			$this->timeout = $timeout;
 		}
 		
+		/**
+		 * @param mixed $date
+		 *
+		 * @return void
+		 */
 		protected function normalizeDate (&$date)
 		{
 			$date = Carbon::createFromFormat('Y-m-d', $date);
 		}
 		
+		/**
+		 * @return array|mixed
+		 */
 		public function currencies ()
 		{
 			return $this->cache->remember('mnb.currencies', $this->timeout, function ()
@@ -31,11 +53,22 @@
 			});
 		}
 		
+		/**
+		 * @param string $code
+		 *
+		 * @return bool
+		 */
 		public function hasCurrency ($code)
 		{
 			return in_array($code, $this->currencies());
 		}
 		
+		/**
+		 * @param string $code
+		 * @param mixed $date
+		 *
+		 * @return array|mixed
+		 */
 		public function exchangeRate ($code, &$date = null)
 		{
 			return $this->cache->remember("mnb.currencies.rate.$code", $this->timeout, function () use (&$code, &$date)
@@ -44,6 +77,11 @@
 			});
 		}
 		
+		/**
+		 * @param mixed $date
+		 *
+		 * @return array|mixed
+		 */
 		public function exchangeRates (&$date = null)
 		{
 			return $this->cache->remember("mnb.currencies.rate", $this->timeout, function () use (&$date)
@@ -52,6 +90,12 @@
 			});
 		}
 		
+		/**
+		 * @param string $code
+		 * @param mixed $date
+		 *
+		 * @return Model\Currency|null
+		 */
 		public function currentExchangeRate ($code, &$date = null)
 		{
 			$result = $this->client->currentExchangeRate($code, $date);
@@ -60,6 +104,11 @@
 			return $result;
 		}
 		
+		/**
+		 * @param mixed $date
+		 *
+		 * @return array|Model\Currency[]
+		 */
 		public function currentExchangeRates (&$date = null)
 		{
 			$result = $this->client->currentExchangeRates($date);
